31f82886c419452c281988776da451bd0a0da9dc
Carlos A. Gomes <carlos-algms@users.noreply.github.com>
2016-09-26 14:38:13 -0300

    Minor improvements

--- postfix-track-sasl-username.js
+++ postfix-track-sasl-username.js
@@ -1,14 +1,12 @@
 var readline = require('readline');
 var fs = require('fs');
 
-// var FILE_PATH = '/var/log/' + (process.argv[2] || 'mail.log');
-var FILE_PATH = './' + (process.argv[2] || 'mail.log');
+var FILE_PATH = '/var/log/' + (process.argv[2] || 'mail.log');
+// var FILE_PATH = './' + (process.argv[2] || 'mail.log');
 var users = [];
-var lineReader = readline.createInterface({
-  input: fs.createReadStream(FILE_PATH)
-});
+var lineReader = readline.createInterface({ input: fs.createReadStream(FILE_PATH) });
 
-console.info('File path: ', FILE_PATH);
+console.log('File path: ', FILE_PATH);
 
 lineReader.on('line', function onLineUsers(line) {
   var saslUsername = line.match(/sasl_username=(.*)/);
@@ -25,10 +23,7 @@ lineReader.on('line', function onLineUsers(line) {
   user.codes.push(code[0]);
 });
 
-lineReader.on('close', function onCloseUsers() {
-  readOccurrences();
-});
-
+lineReader.on('close', readOccurrences);
 
 function getUser(username) {
   var user = null;
@@ -61,19 +56,16 @@ function getUser(username) {
 
 
 function readOccurrences() {
-  var postfixDataRegex = /([A-Z][a-z]{2}\s+[0-9]{1,2} [0-9]{2}:[0-9]{2}:[0-9]{2}) .* postfix\/(.*)\[([0-9]+)\]/;
+  var postfixDataRegex = /([a-z]{2}\s+\d{1,2} \d{2}:\d{2}:\d{2}) .* postfix\/(.*)\[(\d+)\]/i;
   var hostIpRegex = /client=(.*?)\[([0-9\.]+)\]/i;
   var emailToRegex = /to=<(.*?)>/i;
   var emailFromRegex = /from=<(.*?)>/i;
 
-  lineReader = readline.createInterface({
-    input: fs.createReadStream(FILE_PATH)
-  });
+  lineReader = readline.createInterface({ input: fs.createReadStream(FILE_PATH) });
 
   lineReader.on('line', function onLineOcurrence(line) {
     var matchedCode = line.match(/[A-Z0-9]{10}/);
     var code;
-
     if (!matchedCode || !matchedCode[0]) {
       return;
     }
@@ -104,7 +96,7 @@ function readOccurrences() {
 
           if (hostIp) {
             occurrence.ip.push(hostIp[2]);
-            userAddIp(user, hostIp[2]);
+            userAddProp('ips', user, hostIp[2]);
           }
           break;
 
@@ -113,7 +105,7 @@ function readOccurrences() {
 
           if (email && email[1]) {
             occurrence.from.push(email[1]);
-            userAddFrom(user, email[1]);
+            userAddProp('froms', user, email[1]);
           } else if (line.indexOf('removed') === -1) {
             console.log('************** email FROM not found: ', line);
           }
@@ -124,7 +116,7 @@ function readOccurrences() {
 
           if (email && email[1]) {
             occurrence.to.push(email[1]);
-            userAddTo(user, email[1]);
+            userAddProp('tos', user, email[1]);
           } else {
             console.log('************** email TO not found: ', line);
           }
@@ -189,39 +181,19 @@ function ordenar() {
   });
 }
 
-
-function userAddIp(user, ip) {
-  if (user.ips.indexOf(ip) === -1) {
-    user.ips.push(ip);
+function userAddProp(prop, user, data) {
+  if (user[prop].indexOf(data) === -1) {
+    user[prop].push(data);
   }
 }
-
-function userAddTo(user, email) {
-  if (user.tos.indexOf(email) === -1) {
-    user.tos.push(email);
-  }
-}
-
-function userAddCode(user, code) {
-  if (user.codes.indexOf(oc.code) === -1) {
-    user.codes.push(oc.code);
-  }
-}
-
-function userAddFrom(user, email) {
-  if (user.froms.indexOf(email) === -1) {
-    user.froms.push(email);
-  }
-}
-
 
 function printResult() {
   users.forEach(function printUser(user) {
     console.log('=====================================\n');
     console.log(user.count, user.name);
-    console.log( '\n ** FROM:\n   ', user.froms.join('\n    ') );
-    console.log( '\n ** IPs:\n   ', user.ips.join('\n    ') );
-    console.log( '\n ** TO:', user.tos.length, '\n   ', user.tos.join('\n    ') );
+    console.log('\n ** FROM:\n   ', user.froms.join('\n    '));
+    console.log('\n ** IPs:\n   ', user.ips.join('\n    '));
+    console.log('\n ** TO:', user.tos.length, '\n   ', user.tos.join('\n    '));
     console.log('\n\n\n\n');
   });
 }
